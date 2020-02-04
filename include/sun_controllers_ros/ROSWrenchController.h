a5dc6f891efb9a4debcf93c72fe62fcdb0e3d339
Marco Costanzo <marco.costanzo@unicampania.it>
2020-02-04 11:05:08 +0100

    clang-format

--- include/sun_controllers_ros/ROSWrenchController.h
+++ include/sun_controllers_ros/ROSWrenchController.h
@@ -17,154 +17,147 @@
 #ifndef SUN_ROS_WRENCH_CONTROLLER
 #define SUN_ROS_WRENCH_CONTROLLER
 
-#include "ros/ros.h"
-#include "geometry_msgs/WrenchStamped.h"
 #include "geometry_msgs/TwistStamped.h"
+#include "geometry_msgs/WrenchStamped.h"
+#include "ros/ros.h"
 #include "std_srvs/SetBool.h"
 
 #define NUM_ZERO_VEL_COMMANDS_TO_SEND 20
 #define MEAN_NUM_SAMPLES 50
 #define DEFAULT_GAINS 0.0
 
-namespace sun{
-class ROSWrenchController
-{
+namespace sun {
+class ROSWrenchController {
 
-    protected:
-    //Protected members
+protected:
+  // Protected members
 
-    //true if a new wrench measure arrived
-    bool wrench_measure_arrived_;
-    
-    //true if the controller is enabled_
-    bool enabled_;
+  // true if a new wrench measure arrived
+  bool wrench_measure_arrived_;
 
-    //Wrench Controller Gains
-    // [fx,fy,fz,taux,tauy,tauz]
-    double gains_[6];
+  // true if the controller is enabled_
+  bool enabled_;
 
-    //Wrench measure and command as rosmsg
-    geometry_msgs::WrenchStamped wrench_measure_, wrench_command_;
+  // Wrench Controller Gains
+  // [fx,fy,fz,taux,tauy,tauz]
+  double gains_[6];
 
-    //Publisher for the twist commands
-    ros::Publisher pub_twist_control_;
-    
-    //Command and measure wrench subscribers
-    ros::Subscriber sub_wrench_command_, sub_wrench_measure_;
+  // Wrench measure and command as rosmsg
+  geometry_msgs::WrenchStamped wrench_measure_, wrench_command_;
 
-    //ServiceServer set enable
-    ros::ServiceServer srv_server_set_enable_;
+  // Publisher for the twist commands
+  ros::Publisher pub_twist_control_;
 
-    //String for topic and service names
-    std::string wrench_command_topic_str_, 
-                wrench_measure_topic_str_, 
-                twist_command_topic_str_,
-                service_set_enable_str_;
+  // Command and measure wrench subscribers
+  ros::Subscriber sub_wrench_command_, sub_wrench_measure_;
 
-    public:
-    //Public Constructor
+  // ServiceServer set enable
+  ros::ServiceServer srv_server_set_enable_;
 
-    /*
-        Constructor
-        Initialize the controller
-    */
-    ROSWrenchController(
-        ros::NodeHandle& nh_public,
-        ros::NodeHandle& nh_private
-        );
+  // String for topic and service names
+  std::string wrench_command_topic_str_, wrench_measure_topic_str_,
+      twist_command_topic_str_, service_set_enable_str_;
 
-    public:
-    //Public Methods
+public:
+  // Public Constructor
 
-    /*
-        Stop the node
-        use this in a SigintHandler to handle CTRL-C
-    */
-    void stop();
+  /*
+      Constructor
+      Initialize the controller
+  */
+  ROSWrenchController(ros::NodeHandle &nh_public, ros::NodeHandle &nh_private);
 
-    /*
-        Compute the mean of the measures
-        using num_samples samples
-        output has not the header set
-    */
-    geometry_msgs::WrenchStamped compute_measure_mean(int num_samples);
+public:
+  // Public Methods
 
-    /*
-        Compute the control twist output
-        Return a TwistStamped Message (header not set)
-    */
-    geometry_msgs::TwistStamped compute_control();
+  /*
+      Stop the node
+      use this in a SigintHandler to handle CTRL-C
+  */
+  void stop();
 
-    /*
-        Spin until new measure
-    */
-    void wait_wrench_measure();
+  /*
+      Compute the mean of the measures
+      using num_samples samples
+      output has not the header set
+  */
+  geometry_msgs::WrenchStamped compute_measure_mean(int num_samples);
 
-    /*
-        Get params from the parameter server
-    */
-    void get_ros_params(ros::NodeHandle& nh_private);
+  /*
+      Compute the control twist output
+      Return a TwistStamped Message (header not set)
+  */
+  geometry_msgs::TwistStamped compute_control();
 
-    /*
-        Get Controller gains from the parameter server
-    */
-    void get_ros_params_gains(ros::NodeHandle& nh_private);
+  /*
+      Spin until new measure
+  */
+  void wait_wrench_measure();
 
-    /*
-        Get topics and services name from the parameter server
-    */
-    void get_ros_params_topics_services_name(ros::NodeHandle& nh_private);
+  /*
+      Get params from the parameter server
+  */
+  void get_ros_params(ros::NodeHandle &nh_private);
 
-    /*
-        Print the controller params
-    */
-    void print_params();
+  /*
+      Get Controller gains from the parameter server
+  */
+  void get_ros_params_gains(ros::NodeHandle &nh_private);
 
-    protected:
-    //Protected Methods
+  /*
+      Get topics and services name from the parameter server
+  */
+  void get_ros_params_topics_services_name(ros::NodeHandle &nh_private);
 
-    /*
-    Service Callbk - Set Enable
-    Set enable state of the Controller
-    */
-    bool srv_server_set_enabled_cb_(
-        std_srvs::SetBool::Request& request, 
-        std_srvs::SetBool::Response& response
-    );
+  /*
+      Print the controller params
+  */
+  void print_params();
 
-    /*
-        Command cb
-        Update the command
-    */
-    void wrench_command_cb_( const geometry_msgs::WrenchStamped::ConstPtr& msg );
+protected:
+  // Protected Methods
 
-    /*
-        Measure CB
-        Update the measure and call the controller
-    */
-    void wrench_measure_cb_(const geometry_msgs::WrenchStamped::ConstPtr& msg);
+  /*
+  Service Callbk - Set Enable
+  Set enable state of the Controller
+  */
+  bool srv_server_set_enabled_cb_(std_srvs::SetBool::Request &request,
+                                  std_srvs::SetBool::Response &response);
 
-    /*
-        Append the stamp to the control message
-        and publish it 
-    */
-    void publish_control_(geometry_msgs::TwistStamped msg);
+  /*
+      Command cb
+      Update the command
+  */
+  void wrench_command_cb_(const geometry_msgs::WrenchStamped::ConstPtr &msg);
 
-    /*
-        Called before stop
-         - call the user prestop callbk (if present)
-         - Publish ZERO vel
-    */
-    void pre_stop_();
+  /*
+      Measure CB
+      Update the measure and call the controller
+  */
+  void wrench_measure_cb_(const geometry_msgs::WrenchStamped::ConstPtr &msg);
 
-    /*
-        Called before start
-         - call the user prestart callbk (if present)
-         - set the initial command equal to the mean of input (to have an initial Zero error)
-    */
-    void pre_start_();
+  /*
+      Append the stamp to the control message
+      and publish it
+  */
+  void publish_control_(geometry_msgs::TwistStamped msg);
 
-}; //class
-} //namespace sun
+  /*
+      Called before stop
+       - call the user prestop callbk (if present)
+       - Publish ZERO vel
+  */
+  void pre_stop_();
+
+  /*
+      Called before start
+       - call the user prestart callbk (if present)
+       - set the initial command equal to the mean of input (to have an initial
+     Zero error)
+  */
+  void pre_start_();
+
+}; // class
+} // namespace sun
 
 #endif
--- src/wrench_controller_node.cpp
+++ src/wrench_controller_node.cpp
@@ -1,39 +1,36 @@
-#include "sun_controllers_ros/ROSWrenchController.h"
 #include "signal.h"
+#include "sun_controllers_ros/ROSWrenchController.h"
 #include <memory>
 
-//C++11 patch
-namespace patch{
-template<typename T, typename... Args>
-std::unique_ptr<T> make_unique(Args&&... args) {
-    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+// C++11 patch
+namespace patch {
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args &&... args) {
+  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 }
 
 std::unique_ptr<sun::ROSWrenchController> force_controller;
 
-void mySigintHandler(int sig){ 
-    force_controller->stop();
-    ros::shutdown();
+void mySigintHandler(int sig) {
+  force_controller->stop();
+  ros::shutdown();
 }
 
-int main(int argc, char *argv[])
-{
-    //ROS INIT - Use a coustom signinthandler
-    ros::init(argc,argv, "force_controller",ros::init_options::NoSigintHandler);
-    signal(SIGINT, mySigintHandler);
-     
-    ros::NodeHandle nh_private("~");
-    ros::NodeHandle nh_public;
+int main(int argc, char *argv[]) {
+  // ROS INIT - Use a coustom signinthandler
+  ros::init(argc, argv, "force_controller", ros::init_options::NoSigintHandler);
+  signal(SIGINT, mySigintHandler);
 
-    force_controller = patch::make_unique<sun::ROSWrenchController>(
-        nh_public,
-        nh_private
-    );
+  ros::NodeHandle nh_private("~");
+  ros::NodeHandle nh_public;
 
-    force_controller->print_params();
+  force_controller =
+      patch::make_unique<sun::ROSWrenchController>(nh_public, nh_private);
 
-    ros::spin();
+  force_controller->print_params();
 
-    return 0;
+  ros::spin();
+
+  return 0;
 }
